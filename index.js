9c0a6def95e23911b94ffcde8c3e65fcc61734cf
eason <hatebutter83@gmail.com>
2018-07-31 17:01:56 +0800

    v0.0.1

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNClickforce } = NativeModules;
+
+export default RNClickforce;
---
+++ ios/RNClickforce.m
@@ -0,0 +1,13 @@
+
+#import "RNClickforce.h"
+
+@implementation RNClickforce
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
